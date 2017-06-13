019cddfb18b107205b8d6d6b6c6c401c971ebd9d
YuJhon <3291371805@qq.com>
2017-06-13 11:54:59 +0800

    调整一下app.js中中间件的引用顺序

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var path = require('path');
-
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
+
 // rewrite to load static resources
 app.use(express.static(path.join(__dirname, 'public')));
 
