2994f72ae313c7d8b07a44a6e4242f646fbbf6fd
YuJhon <3291371805@qq.com>
2017-06-13 21:37:24 +0800

    Travis-ci 引入到项目中

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(function (req, res, next) {
+  console.log('CurrentTime:', Date.now());
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
--- config/db.js
+++ config/db.js
@@ -1,25 +1,20 @@
 var mongoose = require('mongoose'),
     DB_URL = 'mongodb://localhost:27017/movie';
 
+/** 连接到MongoDB **/
 mongoose.connect(DB_URL);
 
-/**
-  * 连接成功
-  */
+/** 连接成功 **/
 mongoose.connection.on('connected', function () {    
     console.log('Mongoose connection open to ' + DB_URL);  
 });    
 
-/**
- * 连接异常
- */
+/** 连接异常 **/
 mongoose.connection.on('error',function (err) {    
     console.log('Mongoose connection error: ' + err);  
 });    
  
-/**
- * 连接断开
- */
+/** 链接断开 **/
 mongoose.connection.on('disconnected', function () {    
     console.log('Mongoose connection disconnected');  
 });    
